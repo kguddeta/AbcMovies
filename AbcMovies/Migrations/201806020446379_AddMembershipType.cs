0980654392250e6353fc8da98c6fd5a9ea8fd75a
kemal Guddeta <kguddeta@yahoo.com>
2018-06-01 23:54:19 -0500

    Migration added

---
+++ AbcMovies/Migrations/201806020446379_AddMembershipType.cs
@@ -0,0 +1,15 @@
+namespace AbcMovies.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AddMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
