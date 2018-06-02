2c2b644350459f4d60b93c75bbdc0f76c6f0087e
kemal Guddeta <kguddeta@yahoo.com>
2018-06-02 03:26:20 -0500

    properties added to movie domain

--- AbcMovies/Models/Movie.cs
+++ AbcMovies/Models/Movie.cs
@@ -1,9 +1,13 @@
-﻿namespace AbcMovies.Models
+﻿using System;
+
+namespace AbcMovies.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public DateTime Releasedate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
