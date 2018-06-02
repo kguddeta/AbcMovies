7b63f380619f0635e8eeed71541b3013774e58b3
kemal Guddeta <kguddeta@yahoo.com>
2018-06-01 22:52:39 -0500

    Movie controller and views added

---
+++ AbcMovies/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AbcMovies.Models;
+
+namespace AbcMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = new Movie(){Name= "Shrek!"};
+            return View(movie);
+        }
+    }
+}
--- AbcMovies/Models/Movie.cs
+++ AbcMovies/Models/Movie.cs
@@ -1,6 +1,6 @@
 ﻿namespace AbcMovies.Models
 {
-    public class Movies
+    public class Movie
     {
         public int Id { get; set; }
         public string Name { get; set; }
