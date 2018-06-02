bfc9ff4bfb69d31aae301348c4852ff289956887
kemal Guddeta <kguddeta@yahoo.com>
2018-06-02 05:45:53 -0500

    added customerFormview, Save and edit action

--- AbcMovies/Controllers/CustomersController.cs
+++ AbcMovies/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using AbcMovies.Models;
+using AbcMovies.ViewModels;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -19,6 +20,38 @@ namespace AbcMovies.Controllers
         {
             _context.Dispose();
         }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id==0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index","Customers");
+        }
+
         public ActionResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
@@ -34,5 +67,17 @@ namespace AbcMovies.Controllers
             return View(customer);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
     }
 }
--- AbcMovies/Models/Customer.cs
+++ AbcMovies/Models/Customer.cs
@@ -10,9 +10,15 @@ namespace AbcMovies.Models
         [Required]
         [StringLength(255)] //Overriting data annotations
         public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
+
+        [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; }
 
 
