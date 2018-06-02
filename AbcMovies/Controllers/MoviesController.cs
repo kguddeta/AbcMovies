24bd5d669a6f924620cfff20c26779a24e30db04
kemal Guddeta <kguddeta@yahoo.com>
2018-06-02 04:22:17 -0500

    Modified Movie controller class, added moview detail view and updated moview index view

--- AbcMovies/Controllers/MoviesController.cs
+++ AbcMovies/Controllers/MoviesController.cs
@@ -1,14 +1,38 @@
 ﻿using AbcMovies.Models;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Web.UI.WebControls;
+using System.Linq;
 
 namespace AbcMovies.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         public ActionResult Index()
         {
-            var movie = new Movie(){Name= "Shrek!"};
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
             return View(movie);
         }
     }
