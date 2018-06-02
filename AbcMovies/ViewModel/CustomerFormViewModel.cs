291ac0574c00f2253fecad30f73e4e89b7e277d3
Kemal Guddeta <kguddeta@yahoo.com>
2018-06-02 05:51:37 -0500

    Customer form view model is created

---
+++ AbcMovies/ViewModel/CustomerFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AbcMovies.Models;
+using System.Collections.Generic;
+
+namespace AbcMovies.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
