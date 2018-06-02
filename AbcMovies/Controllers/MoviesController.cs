63763679a83a2e323f448e6b91fb07d92e0ee9a6
kemal Guddeta <kguddeta@yahoo.com>
2018-06-01 22:59:33 -0500

    Restructuring the viewLayout by creating partial view(_NavBar file)

--- AbcMovies/Controllers/MoviesController.cs
+++ AbcMovies/Controllers/MoviesController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AbcMovies.Models;
 using System.Web.Mvc;
-using AbcMovies.Models;
 
 namespace AbcMovies.Controllers
 {
