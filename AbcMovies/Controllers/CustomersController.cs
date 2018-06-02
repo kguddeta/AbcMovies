f267e7fa2e0d91f642b669dd1af801657482348e
kemal Guddeta <kguddeta@yahoo.com>
2018-06-01 23:11:42 -0500

    added customers controller, and its view

---
+++ AbcMovies/Controllers/CustomersController.cs
@@ -0,0 +1,15 @@
+﻿using AbcMovies.Models;
+using System.Web.Mvc;
+
+namespace AbcMovies.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = new Customer(){Name="John David"};
+            return View(customer);
+        }
+    }
+}
