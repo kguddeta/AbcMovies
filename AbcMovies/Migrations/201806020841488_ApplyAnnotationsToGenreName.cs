46628bb194a8719b157ecb3a3c0e28d83cb6e414
kemal Guddeta <kguddeta@yahoo.com>
2018-06-02 03:43:34 -0500

    Populated GenreTable, and applied annotations to genre name

---
+++ AbcMovies/Migrations/201806020841488_ApplyAnnotationsToGenreName.cs
@@ -0,0 +1,18 @@
+namespace AbcMovies.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnnotationsToGenreName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Genres", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Genres", "Name", c => c.String());
+        }
+    }
+}
--- AbcMovies/Models/Genre.cs
+++ AbcMovies/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace AbcMovies.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbcMovies.Models
 {
     public class Genre
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
     }
