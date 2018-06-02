dec3d7254c72efb024db22ed3e1d141fe71a5337
kemal Guddeta <kguddeta@yahoo.com>
2018-06-01 23:06:12 -0500

    added customer model

---
+++ AbcMovies/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AbcMovies.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+    }
+}
