84bbd8fba5ab5ae90d3910a39f1b4aa612c7f68d
kemal Guddeta <kguddeta@yahoo.com>
2018-06-06 16:14:54 -0500

    customer web api added

---
+++ AbcMovies/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AbcMovies.Dtos;
+using AbcMovies.Models;
+using AutoMapper;
+
+namespace AbcMovies.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ AbcMovies/Dtos/CustomerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AbcMovies.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        
+        [StringLength(255)] 
+        public string Name { get; set; }
+
+  //      [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
